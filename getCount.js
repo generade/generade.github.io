c6c8dd5904b8d194d6c3a5e8a31581596d46bcdc
generade <generade@126.com>
2018-03-12 13:01:27 +0800

    Update getCount.js

--- getCount.js
+++ getCount.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  var onlyId = $("<span id='onlyId'></span>");
+    $("body").append(onlyId);
+    if(w_dd.data.roundOnlyId == undefined || w_dd.data.roundOnlyId.length == 0){
+	  alert("数据获取失败");
+	  return;
+  }
+  else{
+	  $("#onlyId").html(w_dd.data.roundOnlyId);
+  }
   $(".dati").bind("click",function(){
     ajaxGet('game_info/share');
   });
