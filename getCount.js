e48693d8ae01f5d8dd2f673d8e0bf13bf5a2eb68
generade <generade@126.com>
2018-03-15 12:48:26 +0800

    Update getCount.js

--- getCount.js
+++ getCount.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   var onlyId = $("<span id='onlyId'></span>");
     $("body").append(onlyId);
+    try{
     if(w_dd.data.roundOnlyId == undefined || w_dd.data.roundOnlyId.length == 0){
 	  alert("roundOnlyId数据获取失败");
 	  return;
+    }
+    catch(e){console.log(e);}
   }
   else{
 	  $("#onlyId").html(w_dd.data.roundOnlyId);
