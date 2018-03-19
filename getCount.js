f0d7c684af00aac9858adf80fe6b78aa05941393
generade <generade@126.com>
2018-03-19 18:02:12 +0800

    Update getCount.js

--- getCount.js
+++ getCount.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
   $(".dati").bind("click",function(){
     ajaxGet('game_info/share');
   });
- alert("即日起，本程序更换新版本，请到”https://pan.baidu.com/s/1gjDgH8UfCBKm-22VlhvDLg“下载，提取码：c8yu。然后按照说明使用。")
+ alert("即日起，本程序更换新版本，请到”https://pan.baidu.com/s/1IBH3k0z7kGKskplxfVjUYg“下载，提取码：1y9y。然后按照说明使用。")
 });
