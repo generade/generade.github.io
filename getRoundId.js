0c2fc9723ab95d32ad2f0bfea6e2976f67d15520
generade <generade@126.com>
2018-03-19 17:32:11 +0800

    Update getRoundId.js

--- getRoundId.js
+++ getRoundId.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
     try{
     if(w_dd.data.roundOnlyId == undefined || w_dd.data.roundOnlyId.length == 0){
 	  alert("答题信息数据获取失败，请刷新页面重试！");
-	    $("#info").html("免费授权码：xHYy555A")
      }
   else{
 	  $("#onlyId").html(w_dd.data.roundOnlyId);
+	  setTimeout('$("#info").html("免费授权码：xHYy555A")',3000);
   }
     }
 	 catch(e){console.log(e);}
