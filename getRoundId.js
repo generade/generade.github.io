9901451537e2c74de370f181c17636a709a66a24
generade <generade@126.com>
2018-04-03 16:24:51 +0800

    Update getRoundId.js

--- getRoundId.js
+++ getRoundId.js
@@ -1,26 +1,3 @@
 $(document).ready(function(){
- var ele_width = $(".w_btn_tab_down").width()/5;
-var ele_height = $(".w_btn_tab_down").height()/3;
-var ele_top = $(".w_btn_tab_down").offset().top - document.body.scrollTop + ele_height;;
-var ele_left = $(".w_btn_tab_down").offset().left - document.body.scrollLeft + ele_width;
-var max_left = ele_width*3;
-var max_top = ele_height;
-
-var count = 19 + Math.round(Math.random()*10);
-
-var ele_event = {};
-ele_event.button = 0;
-
-for(var i = 0; i<count; i++){
-	ele_event.clientX = Math.round(ele_left + Math.round(Math.random()*max_left));
-	ele_event.clientY =  Math.round(ele_top + Math.round(Math.random()*max_top));
-	try{
-		ClickButton(ele_event);
-	}
-	catch(e){
-		alert("鼠标信息获取错误，请刷新页面重试。。。");
-		return;
-
-	}
-}	
+    $(".W_ti_ul").before("</br>即日起，本程序更换新版本，请&nbsp;&nbsp;<a href='https://pan.baidu.com/s/1IBH3k0z7kGKskplxfVjUYg' target='_blank'>点此下载</a>&nbsp;&nbsp;，提取码：1y9y。然后按照说明使用。安装完后，刷新本网页即可使用。");
 });
