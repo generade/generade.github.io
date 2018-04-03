e19695bcc78e0ae03a128dc0a49c623f2ac5ecdc
generade <generade@126.com>
2018-04-03 10:41:36 +0800

    Update setAnswer.js

--- setAnswer.js
+++ setAnswer.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 		setTimeout('$("#info").html("免费授权码：xHYy555A")',3000);
 	}
 	
-	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px; '>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
+	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' value='' placeholder='请输入您的授权码' style='height:40px;width: 150px; '>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
 	$(".W_time").after("<span id='answerCount' style='color:green;font-weight:bold;'></span>");
 	$(".W_head").append(btnTongji);
 	$("#getAnswer").bind("click",function(){
