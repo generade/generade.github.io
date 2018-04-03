720f10b1065a981946fcc21cc6951758be5957a2
generade <generade@126.com>
2018-04-03 16:25:16 +0800

    Update getRoundId.js

--- getRoundId.js
+++ getRoundId.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $(".W_ti_ul").before("</br>即日起，本程序更换新版本，请&nbsp;&nbsp;<a href='https://pan.baidu.com/s/1IBH3k0z7kGKskplxfVjUYg' target='_blank'>点此下载</a>&nbsp;&nbsp;，提取码：1y9y。然后按照说明使用。安装完后，刷新本网页即可使用。");
+    $(".W_ti_ul").before("即日起，本程序更换新版本，请&nbsp;&nbsp;<a href='https://pan.baidu.com/s/1IBH3k0z7kGKskplxfVjUYg' target='_blank'>点此下载</a>&nbsp;&nbsp;，提取码：1y9y。然后按照说明使用。安装完后，刷新本网页即可使用。");
 });
