64e92247bdcc145e367a22bba517faff528f2808
generade <generade@126.com>
2018-05-17 10:30:51 +0800

    Update autoShare.js

--- autoShare.js
+++ autoShare.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  var thisHref = loaction.href;
-  if(thisHref == "https://www.dtdjzx.gov.cn/member/") this.location.href = 'http://xxjs.dtdjzx.gov.cn/';
   $(".dati").bind("click",function(){
     ajaxGet('game_info/share');
   });
