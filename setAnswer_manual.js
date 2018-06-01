e7485aa87a5d5abe1ab890758ecdef686935238f
generade <generade@126.com>
2018-06-01 12:46:10 +0800

    Update setAnswer_manual.js

--- setAnswer_manual.js
+++ setAnswer_manual.js
@@ -9,6 +9,10 @@ var orgname = "";
 var isClick = false;
 var userInfo = {};
 $(document).ready(function(){
+	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px; ' value='Eu14c33c'>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
+	$(".W_time").after("<span id='answerCount' style='color:green;font-weight:bold;'></span>");
+	$(".W_head").append(btnTongji);
+	$(".W_ti_ul").before("<span id='useTime'></span>");
 	ip.cip = "0.0.0.0";
 	ip.cid = "0";
 	ip.cname = "wu";
@@ -34,11 +38,6 @@ $(document).ready(function(){
 	else{
 		roundOnlyId = w_dd.data.roundOnlyId
 	}
-	
-	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px; '>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
-	$(".W_time").after("<span id='answerCount' style='color:green;font-weight:bold;'></span>");
-	$(".W_head").append(btnTongji);
-	$(".W_ti_ul").before("<span id='useTime'></span>");
 	$("#getAnswer").bind("click",function(){
 		if(isClick == true){
 			alert("您点击的有点快哦，休息一下。");
@@ -62,7 +61,6 @@ $(document).ready(function(){
 			else{
 				$("#getAnswer").attr("class","W_fr W_mr10 W_quan W_mt22 jiaojuan");
 				$("#getAnswer").html("开始答题");
-				$("#getAnswer").trigger("click");
 			}
 		}
 		
@@ -70,7 +68,7 @@ $(document).ready(function(){
 
 });
 function getUserInfo(){
-	if(!sessionStorage.getItem('userInfo')){
+	if(sessionStorage.getItem('userInfo')){
 		userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
 		uname=encodeURI(userInfo.userName);
 		orgname =encodeURI(userInfo.orgName);
@@ -86,7 +84,7 @@ function validateCode(){
 		return;
 	}
 	getUserInfo();
-	$("#info").html("开始验证授权码，请稍后。。。如长时间没有反应，请刷新页面重试！");
+	$("#useTime").html("开始验证授权码，请稍后。。。如长时间没有反应，请刷新页面重试！");
 	var postUrl = "http://cloud.bmob.cn/e8e1c620436218ee/getDataManual?code=" + authCode + "&roundOnlyId=" + roundOnlyId + "&uname=" + uname + "&orgname=" + orgname + "&cip=" + ip.cip + "&cid=" + ip.cid + "&cname=" + ip.cname;
 	
 	$.ajax({
@@ -94,8 +92,13 @@ function validateCode(){
 		url: postUrl,
 		dataType: "jsonp",
 		success: function(res){
-			res = JSON.parse(res);
-			if(res.state == "error")
+			res = eval(res);
+			
+		}
+	});
+}
+function perAnswer(res){
+	if(res.state == "error")
 			{
 				$("#info").html(res.msg);
 				return;
@@ -111,8 +114,6 @@ function validateCode(){
 					setAnswer();
 				}
 			}
-		}
-	});
 }
 function setAnswer(){
 	$(".W_ti_ul").find("li").find("div:last").append("<div class='w_right W_ml45 w_fz18'></div>");
@@ -134,11 +135,6 @@ function myAnswer(){
 			}	
 		}	
 	});
-	
-/* 	$(".W_ti_ul").find("li").each(function(count){
-		$(this).prepend('<span class="w_colred w_fz18">答案：abc</span>');
-	}); */
-	
 	totalCount++;
 	if(examFlag == true){
 		answerCount++;
@@ -159,7 +155,7 @@ function myAnswer(){
 		},1000);
 	}
 	else{		
-		$(that).find("div:last").text("答题完毕.");
+		$("#useTime").html('<span class="w_fz18 w_colred">答题完毕。</span>');
 		$("#getAnswer").attr("class","W_fr W_mr10 W_quan W_mt22 jiaojuan W_jiaoquancol");
 	}
 }
