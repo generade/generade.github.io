e2f3e2f06c85222f11bab9a9dd6885ce7491093c
generade <generade@126.com>
2018-06-01 12:40:01 +0800

    Update setAnswer_manual.js

--- setAnswer_manual.js
+++ setAnswer_manual.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
     }
 	else{
 		roundOnlyId = w_dd.data.roundOnlyId
-		setTimeout('$("#info").html("免费授权码：xHYy555A")',2000); 
 	}
 	
 	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px; '>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
