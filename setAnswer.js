386d006fc48cad6e98d95c2b3a84949b801d6c8a
generade <generade@126.com>
2018-06-01 11:59:08 +0800

    Update setAnswer.js

--- setAnswer.js
+++ setAnswer.js
@@ -9,7 +9,7 @@ var orgname = "";
 var isClick = false;
 var userInfo = {};
 $(document).ready(function(){
-	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px;' value='5XCVU557'>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
+	var btnTongji = $("<span id='ad'></span>&nbsp;&nbsp;授权码：<input type='text' id='authCode' placeholder='请输入您的授权码' style='height:40px;width: 150px;' value=''>&nbsp;&nbsp;<span id='info'></span><span class='W_fr W_mr10 W_quan W_mt22 jiaojuan  W_jiaoquancol' id='getAnswer'></span>");
 	$(".W_time").after("<span id='answerCount' style='color:green;font-weight:bold;'></span>");
 	$(".W_head").append(btnTongji);
 	$(".W_ti_ul").before("<span id='useTime'></span>");
@@ -49,7 +49,7 @@ $(document).ready(function(){
 			clearInterval(window.randomTimer);
 			$("#getAnswer").attr("class","W_fr W_mr10 W_quan W_mt22 jiaojuan");
 			$("#getAnswer").html("开始答题");
-			$("#getAnswer").trigger("click");
+
 		}
 		
 	},1000);
