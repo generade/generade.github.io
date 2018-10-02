bd4fe471dff8dbb922c7a6ab4dea65e05adc3686
generade <generade@126.com>
2018-10-02 12:37:44 +0800

    Update insertCode_setRowNumber.js

--- insertCode_setRowNumber.js
+++ insertCode_setRowNumber.js
@@ -1,7 +1,10 @@
-	var childFrameDoc= document.getElementById("myIframe").contentDocument || document.frames["myIframe"].document;
-	var script=childFrameDoc.createElement("script"); 
-	script.setAttribute("type", "text/javascript"); 
-	script.setAttribute("src", "https://generade.github.io/setRowNumber.js"); 
-	var heads = childFrameDoc.getElementsByTagName("head");
-	var bodyf = childFrameDoc.getElementsByTagName("body");
+var childFrameDoc= document.getElementById("myIframe").contentDocument || document.frames["myIframe"].document;
+var script=childFrameDoc.createElement("script"); 
+script.setAttribute("type", "text/javascript"); 
+script.setAttribute("src", "https://generade.github.io/setRowNumber.js"); 
+var heads = childFrameDoc.getElementsByTagName("head");
+var bodyf = childFrameDoc.getElementsByTagName("body");
+setTimeout('loadScript()',2000);
+function loadScript(){
 	bodyf[0].appendChild(script); 
+}
