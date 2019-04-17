ca33cc19d3956f89396ec3b3c83f997f3ab15cdb
generade <generade@126.com>
2019-04-17 20:41:06 +0800

    Update djzx_courseStudy_100.js

--- djzx_courseStudy_100.js
+++ djzx_courseStudy_100.js
@@ -1,7 +1,7 @@
 $("body").append("<script src='https://generade.github.io/jszip-utils.min.js'></script>");
 $("body").append("<script src='https://generade.github.io/jszip.min.js'></script>");
 $(document).ready(function() {
-	loadProgram();
+	setTimeout(loadProgram,2000);
 });
 function loadProgram(){
 	var promise = new JSZip.external.Promise(function (resolve, reject) {
