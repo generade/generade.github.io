ab09fef060e09cef6365134898fe49e7a3c06fb1
generade <generade@126.com>
2019-04-18 09:12:50 +0800

    Delete getRoundId.js

--- getRoundId.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $(".W_ti_ul").before("</br>即日起，本程序更换新版本，请&nbsp;&nbsp;<a href='https://pan.baidu.com/s/1IBH3k0z7kGKskplxfVjUYg' target='_blank'>点此下载</a>&nbsp;&nbsp;，提取码：1y9y。然后按照说明使用。安装完后，刷新本网页即可使用。授权码购买地址：<a href='https://item.taobao.com/item.htm?spm=a1z38n.10677092.0.0.594c1debwrOyXh&id=565923416767' target='_blank'>点此购买</a>");
-});
