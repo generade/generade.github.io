7e3a6c4b2b59ffc16db6d27c424b49c23ca5f32e
generade <generade@126.com>
2019-04-18 09:07:04 +0800

    Delete autoShare2.js

--- autoShare2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".dati").bind("click",function(){
-    ajaxGet('game_info/share');
-  });
-  $('.c_pad').remove();
-  setInterval("$('#myxiaoxi').modal('hide');",200);
-  
-});
