2045cea407fe55e3f770ae9e403119dc1ea74985
generade <generade@126.com>
2020-01-05 13:45:41 +0800

    Update djzx_courseStudy_100.js

--- djzx_courseStudy_100.js
+++ djzx_courseStudy_100.js
@@ -1,5 +1,3 @@
-$("body").append("<script src='https://generade.github.io/jszip-utils.min.js'></script>");
-$("body").append("<script src='https://generade.github.io/jszip.min.js'></script>");
 $(document).ready(function() {
 	setTimeout(loadProgram,1000);
 });
